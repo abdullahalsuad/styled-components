92d2832436c1b2fb3c9e51dd59a0d11dbc484d7c
Harun-Ar- Rashid <suadabdullahal@gmail.com>
2021-08-02 14:50:06 +0600

    70 pre done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-unused-vars */
-
 import Tutorial from './components/Tutorial';
 
 export default function App() {
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,23 @@
 import nerdImage from '../ass/img/illustration.png';
 import Button from './Button';
-import Image from './Image';
-import {Tag} from './styles/Element.styled'
-import {CardContainer, ContenContainer} from './styles/container.styles'
+import {Tag,H1,P,Image} from './styles/Element.styled'
+import {CardContainer, ContentContainer,ButtonContainer} from './styles/container.styles'
 export default function Card() {
     return (
         <CardContainer>
-            <ContenContainer>
-                <Tag >EXCLUSIVE</Tag>
-                <h1>React Styled Components</h1>
-                <p>
+            <ContentContainer><></>
+                <Tag color="#4361ee">EXCLUSIVE</Tag>
+                <H1>React Styled Components</H1>
+                <P>
                     Exclusive React JS Tutorial on Styled Components where you will learn why we
                     need this & how to use it.
-                </p>
-                <div>
+                </P>
+                <ButtonContainer>
                     <Button link="#" text="Watch now" />
                     <Button link="#" text="Github repo" />
-                </div>
-            </ContenContainer>
-            <Image imgSrc={nerdImage} alt="Nerd" width="300px" />
-        </CardContainer>
+                </ButtonContainer>
+            </ContentContainer>
+            <Image src={nerdImage} alt="Nerd" width="300px" />
+        </CardContainer> 
     );
 }
